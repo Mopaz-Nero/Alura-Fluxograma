2cc7b306929dc36c8726045dcb81724b9b144f42
Mopaz-Nero <114631489+Mopaz-Nero@users.noreply.github.com>
2024-09-15 20:17:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,4 @@ avanca.forEach(button => {
         atual.classList.remove('ativo')
         document.getElement(proximoPasso).classList.add('sim');
     })
-})const avanca = document.querySelectorAll('.btn-proximo');
-
-avanca.forEach(button => {
-    buttonn.addEventListener('click', function(){
-        const atual = document.querySelector('.sim')
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
-
-        atual.classList.remove('ativo')
-        document.getElement(proximoPasso).classList.add('sim');
-    })
 })
