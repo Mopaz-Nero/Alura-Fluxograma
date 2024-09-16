ba233ed81f533ef5c9fd0969681c853a9ae69cd4
Mopaz-Nero <114631489+Mopaz-Nero@users.noreply.github.com>
2024-09-15 22:36:09 -0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    buttonn.addEventListener('click', function(){
-        const atual = document.querySelector('.sim')
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo')
-        document.getElement(proximoPasso).classList.add('sim');
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
